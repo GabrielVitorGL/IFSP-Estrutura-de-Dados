36cfecdc728c33345fd6d98efc08d1d5df6d6781
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-08-19 21:33:05 -0300

    adding first example

---
+++ 01 - Slide 16 - Comparacao de 2 numeros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b;
+
+    printf("Digite dois numeros: \n");
+    scanf("%d %d", &a, &b);
+
+    if (a == b) {
+        printf("Os numeros sao iguais \n\n\n");
+    }
+
+    system("pause");
+
+    return 0;
+}
