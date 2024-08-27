cda2ec1324a0c3cee7fc15e5a90a56a20c784ae8
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-08-26 21:16:51 -0300

    adding second class material and files

---
+++ Atividade Aula 2/calculoVantagensDeducoes.c
@@ -0,0 +1,48 @@
+
+
+#include <stdio.h>
+
+float salarioBruto, salarioFamilia, vantagens, INSS, IRPF, Deducoes;
+
+void calculoVantagens(float numeroHoras, float salarioHora, int numeroFilhos, float valorPorFilho);
+void calculoDeducoes(float taxaIR);
+
+int main(){
+    float numeroHoras, salarioHora, valorPorFilho, taxaIR;
+    int numeroFilhos;
+
+    printf("Digite o numero de horas trabalhadas: ");
+    scanf(" %f", &numeroHoras);
+    printf("Digite o salario por hora: ");
+    scanf(" %f", &salarioHora);
+    printf("Digite o numero de filhos: ");
+    scanf(" %d", &numeroFilhos);
+    printf("Digite o valor por filho: ");
+    scanf(" %f", &valorPorFilho);
+    printf("Digite a taxa de IR: ");
+    scanf(" %f", &taxaIR);
+
+    calculoVantagens(numeroHoras, salarioHora, numeroFilhos, valorPorFilho);
+    calculoDeducoes(taxaIR);
+
+    printf("\n\nSalario Bruto: %.2f\n", salarioBruto);
+    printf("Salario Familia: %.2f\n", salarioFamilia);
+    printf("Vantagens: %.2f\n", vantagens);
+    printf("INSS: %.2f\n", INSS);
+    printf("IRPF: %.2f\n", IRPF);
+    printf("Deducoes: %.2f\n", Deducoes);
+
+    return 0;
+}
+
+void calculoVantagens(float numeroHoras, float salarioHora, int numeroFilhos, float valorPorFilho){
+    salarioBruto = numeroHoras * salarioHora;
+    salarioFamilia = numeroFilhos * valorPorFilho;
+    vantagens = salarioBruto + salarioFamilia;
+}
+
+void calculoDeducoes(float taxaIR){
+    INSS = salarioBruto * 0.08;
+    IRPF = salarioBruto * taxaIR;
+    Deducoes = INSS + IRPF;
+}
